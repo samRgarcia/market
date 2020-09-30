1f93598afa43c5a6d587167d8216c6ddb18ee408
samRgarcia <samuelrivas18@gmail.com>
2020-09-30 18:22:28 -0500

    listos

---
+++ platzi-market/src/main/java/com/platzimarket/domain/services/PlatziUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.platzimarket.domain.services;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class PlatziUserDetailsService implements UserDetailsService {
+@Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+    return new User(username="sam","{noop}platzi",new ArrayList<>());
+
+
+
+
+
+}
+
+}
